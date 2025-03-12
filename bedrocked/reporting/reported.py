926ec5591f1fc800a3b7b7521e5254b90a4b90e8
aidan <aidankhogg@gmail.com>
2025-03-12 11:30:24 +0000

    implemented core functionality
    - logging (with loading from file)
    - configuration (environment management, feature flagging)
    - start of work and job management

--- bedrocked/reporting/reported.py
+++ bedrocked/reporting/reported.py
@@ -3,3 +3,6 @@ from bedrocked.reporting.logged import logger
 
 class ReportTypes(Enum):
     logger.error("NotYetImplementedYet: 'Reporter' features are unavailable.")
+
+def report(report_type: str, *args, **kwargs):
+    raise NotImplementedError("The central reporter has not yet been implemented.")
